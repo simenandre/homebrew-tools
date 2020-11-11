32fe0533de87719200dac5a7a9514b8d1c858c23
Simen A. W. Olsen <cobraz@cobraz.no>
2020-11-11 08:26:26 +0100

    Brew formula update for trippl-timely version v2.0.0

--- Formula/trippl-timely.rb
+++ Formula/trippl-timely.rb
@@ -2,16 +2,16 @@
 class TripplTimely < Formula
   desc "Easily import Jira issues to a Tripletex project."
   homepage "https://github.com/cobraz/trippl-timely"
-  version "1.0.2"
+  version "2.0.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/cobraz/trippl-timely/releases/download/v1.0.2/trippl-timely_1.0.2_Darwin_x86_64.tar.gz"
-    sha256 "fcb0716f7e745981ca0d8be2635777e30ec7e45c97da30c966e7655d470ba63e"
+    url "https://github.com/cobraz/trippl-timely/releases/download/v2.0.0/trippl-timely_2.0.0_Darwin_x86_64.tar.gz"
+    sha256 "abe7ab3ed0499f582e08c76696b0b6cb4564ee1114139581bc52bf12ac549e69"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/cobraz/trippl-timely/releases/download/v1.0.2/trippl-timely_1.0.2_Linux_x86_64.tar.gz"
-      sha256 "9dcb72ddb857fd10b7440e40333a966e89ffa0922850c1c79a82808207be58ab"
+      url "https://github.com/cobraz/trippl-timely/releases/download/v2.0.0/trippl-timely_2.0.0_Linux_x86_64.tar.gz"
+      sha256 "d53d811c580361ac63e8fb0cece7b8ead1e0eccc472b79544567f5dd569ae601"
     end
   end
 
