2edd93bb0c3cb7064fc71dcd8e7f2669acb4db5b
Simen A. W. Olsen <cobraz@cobraz.no>
2022-05-06 08:21:09 +0200

    Bump cobraz/xlsx-mxlookup formula

--- Formula/xlsx-mxlookup.rb
+++ Formula/xlsx-mxlookup.rb
@@ -1,8 +1,8 @@
 class XlsxMxlookup < Formula
-  version '1.1.0'
+  version '1.1.1'
   homepage 'https://github.com/cobraz/xlsx-mxlookup'
-  url "https://github.com/cobraz/xlsx-mxlookup/releases/download/v1.1.0/xlsx-mxlookup_darwin_amd64.zip"
-  sha256 'ba8a14b7d8c181b2cc47d0ee25faf88625faa38780f57c6d620f5b909d204c76'
+  url "https://github.com/cobraz/xlsx-mxlookup/releases/download/v1.1.1/xlsx-mxlookup_darwin_amd64.zip"
+  sha256 '34ad993861f5718bd84acd7d7b6e97848f69c4db71dbc96c98d4cfb39a391baf'
   head 'https://github.com/cobraz/xlsx-mxlookup.git'
 
   head do
