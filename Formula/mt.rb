eebf6b39c9bcf0a3d0002446c14320b5fb12e20c
Simen A. W. Olsen <hei@simenandre.no>
2025-03-12 22:28:47 +0100

    Update mt.rb

--- Formula/mt.rb
+++ Formula/mt.rb
@@ -1,6 +1,7 @@
 class Mt < Formula
   desc "Enough talk, show me TODOs"
   homepage "https://github.com/simenandre/mt"
+  url "https://github.com/simenandre/mt/archive/refs/tags/v0.0.1.zip"
   license "Apache-2.0"
 
   def install
